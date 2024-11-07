7e0016124843d22582e9c8647b3fcd6dfeab7417
fujimitu13 <fujii.kotaro.4980@members.co.jp>
2024-11-07 17:25:31 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col, when_matched 
+from snowflake.snowpark import Session
+from snowflake.snowpark.functions import col
+
+# Snowflakeセッションの作成
+session = Session.builder.configs(connection_parameters).create()
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -12,18 +15,17 @@ name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie will be:", name_on_order)
 
 # Snowflakeセッションの取得
-session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 # ユーザーに果物の選択を促す
 ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:'
-    ,my_dataframe.to_pandas()['FRUIT_NAME'].tolist()  # Snowpark DataFrameをPandas DataFrameに変換してリストを取得
-    ,max_selections=5
+    'Choose up to 5 ingredients:',
+    my_dataframe.to_pandas()['FRUIT_NAME'].tolist(),  # Snowpark DataFrameをPandas DataFrameに変換してリストを取得
+    max_selections=5
 )
 
 if ingredients_list:
-    ingredients_string = ' '.join(ingredients_list)  # 選択した果物をカンマ区切りの文字列にする
+    ingredients_string = ', '.join(ingredients_list)  # 選択した果物をカンマ区切りの文字列にする
 
     # SQL文をパラメータ化
     my_insert_stmt = "INSERT INTO smoothies.public.orders (ingredients, name_on_order) VALUES (:1, :2)"  # 正しい列名を使用
